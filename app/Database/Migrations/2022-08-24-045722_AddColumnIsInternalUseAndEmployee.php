31d7c52b6d503fe8ae4f9979e8623eddb1e5cb16
Tintapen <okiper12@gmail.com>
2022-08-24 14:09:41 +0700

    Add column migration script md_employee_id and isinternaluse

---
+++ app/Database/Migrations/2022-08-24-045722_AddColumnIsInternalUseAndEmployee.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddColumnIsInternalUseAndEmployee extends Migration
+{
+    protected $DBGroup = 'default';
+
+    public function up()
+    {
+        $fields = [
+            'md_employee_id'    => [
+                'type'          => 'INT',
+                'after'         => 'md_supplier_id',
+                'constraint'    => 6,
+                'null'          => false
+            ]
+        ];
+
+        $this->forge->addColumn('trx_quotation', $fields);
+
+        $fields2 = [
+            'md_employee_id'    => [
+                'type'          => 'INT',
+                'after'         => 'md_supplier_id',
+                'constraint'    => 6,
+                'null'          => false
+            ],
+            'isinternaluse'     => [
+                'type'          => 'CHAR',
+                'after'         => 'expenseno',
+                'contraint'     => 1,
+                'null'          => false,
+                'default'       => 'N'
+            ]
+        ];
+
+        $this->forge->addColumn('trx_receipt', $fields2);
+    }
+
+    public function down()
+    {
+        $fields = ['md_employee_id'];
+
+        $this->forge->dropColumn('trx_receipt', $fields);
+
+        $fields2 = ['md_employee_id', 'isinternaluse'];
+
+        $this->forge->dropColumn('trx_quotation', $fields2);
+    }
+}
