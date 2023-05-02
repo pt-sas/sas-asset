d625db82e5a7f58d41cac8aa6a70c28baeec6c93
Tintapen <okiper12@gmail.com>
2023-05-02 08:51:30 +0700

    Modify report movement detail

--- app/Views/report/movement/v_movementdetail.php
+++ app/Views/report/movement/v_movementdetail.php
@@ -3,63 +3,54 @@
 <?= $this->section('content'); ?>
 <form id="parameter_movementdetail">
     <div class="card-body">
-
         <div class="form-group row">
             <label for="md_employee_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Employee From </label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-employee" name="employee_from"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_employee_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Employee To </label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-employee" name="employee_to"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Division From </label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-division" name="division_from"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Division To </label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-division" name="division_to"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_branch_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Branch From</label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-branch" name="branch_from"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_branch_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Branch To</label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-branch" name="branch_to"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_room_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Room From</label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-room" name="room_from"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_room_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Room To</label>
             <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
                 <select class="form-control select-room" name="room_to"></select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="md_status_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Status </label>
             <div class="col-lg-6 col-md-9 col-sm-8">
@@ -71,7 +62,6 @@
                 </select>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="movementdate" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Movement Date</label>
             <div class="col-lg-6 col-md-9 col-sm-8 ">
@@ -84,6 +74,5 @@
             </div>
         </div>
     </div>
-
 </form>
 <?= $this->endSection() ?>
