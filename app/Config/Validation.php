d6264c66a60c727e565609d6a350bf2cb2d83e7d
Tintapen <okiper12@gmail.com>
2023-06-12 09:57:14 +0700

    Remove validation alert

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -178,10 +178,6 @@ class Validation
         'md_room_id'         => [
             'label'            =>    'Room',
             'rules'            =>    'required'
-        ],
-        'alert.*'           => [
-            'label'         => 'Alert',
-            'rules'         => 'required'
         ]
     ];
 
