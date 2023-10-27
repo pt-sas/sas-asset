78fddd5a4a230f99550a892e6ca2d2792d1d472b
Tintapen <okiper12@gmail.com>
2023-10-27 14:07:44 +0700

    Add field parameter created_by

--- app/Views/report/opname/v_opname.php
+++ app/Views/report/opname/v_opname.php
@@ -52,6 +52,13 @@
                 </div>
             </div>
         </div>
+        <div class="form-group row">
+            <label for="created_by" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Created By</label>
+            <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
+                <select class="form-control multiple-select-user" name="created_by">
+                </select>
+            </div>
+        </div>
     </div>
 </form>
 <?= $this->endSection() ?>
