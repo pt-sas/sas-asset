87b0639634e3b8e7746e616c04b449d52aa036f4
Tintapen <okiper12@gmail.com>
2024-03-26 16:02:17 +0700

    perf(release): Change field type

--- app/Views/report/assetdetail/v_assetdetail.php
+++ app/Views/report/assetdetail/v_assetdetail.php
@@ -95,15 +95,14 @@
                 </div>
             </div>
         </div>
-        <div class="form-check">
-            <div class="row">
-                <label class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Spare </label>
-                <div class="col-lg-4 col-md-9 col-sm-8 d-flex align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="isspare" name="isspare">
-                        <label class="custom-control-label" for="isspare"> </label>
-                    </div>
-                </div>
+        <div class="form-group row">
+            <label for="isspare" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Spare </label>
+            <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
+                <select class="form-control select2" name="isspare">
+                    <option value="0">All</option>
+                    <option value="Y">Yes</option>
+                    <option value="N">No</option>
+                </select>
             </div>
         </div>
     </div>
